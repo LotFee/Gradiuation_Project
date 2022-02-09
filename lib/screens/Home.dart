1d7925e61151ec39baae90eef7bc4faa060c1ebf
LotFee <lotfyua@gmail.com>
2022-02-09 20:25:29 +0200

    home edit1

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -6,14 +6,49 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: WillPopScope(
-          onWillPop: () async => false,
-          child: Container(
-            child: Text("Coming soon" ,style: TextStyle(color:Color(0xff1f95a1) ,fontSize: 30,fontWeight: FontWeight.bold ),),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+
+        appBar: AppBar(
+          foregroundColor: Color(0xff1f95a1),
+          backgroundColor: Colors.white,
+          backwardsCompatibility: false,
+          title: Center(child: Text("Home", style: TextStyle(color:Color(0xff1f95a1),fontWeight: FontWeight.bold ),)),
+          actions: [
+              GestureDetector(onTap: (){},child: Icon(Icons.search,color: Color(0xff1f95a1),size: 35)),
+        ],
+        ),
+         drawer: Drawer(
+
+         ),
+        floatingActionButton:FloatingActionButton(
+          onPressed: (){
+
+          },
+          backgroundColor: Color(0xff1f95a1),
+          child: Icon(Icons.add), //icon inside button
+        ),
+
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
+
+        bottomNavigationBar: BottomAppBar(
+          color:Colors.white,
+          shape: CircularNotchedRectangle(),
+          notchMargin: 7,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(icon: Icon(Icons.home, color: Color(0xff1f95a1),size: 30,), onPressed: () {},),
+              IconButton(icon: Icon(Icons.favorite, color: Color(0xff1f95a1),size: 30), onPressed: () {},),
+              IconButton(icon: Icon(Icons.notifications, color: Color(0xff1f95a1),size: 30), onPressed: () {},),
+              IconButton(icon: Icon(Icons.person, color: Color(0xff1f95a1),size: 30), onPressed: () {},),
+            ],
           ),
         ),
+
       ),
     );
   }
