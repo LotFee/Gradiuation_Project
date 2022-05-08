c45b52900e1454ca9c7e9c5858c331e6a880a649
LotFee <lotfyua@gmail.com>
2022-05-08 23:40:36 +0200

    delete comment done

--- lib/screens/comments.dart
+++ lib/screens/comments.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart'as http;
 import 'package:provider/provider.dart';
 
+import '../provider/current_user.dart';
 import '../providers/Flats.dart';
 
 class Comments extends StatefulWidget {
@@ -20,28 +21,14 @@ class _CommentsState extends State<Comments> {
   String flatID='';
   List filedata = [
     {
-      'name': 'Adeleye Ayodeji',
+      'name': 'No Commented Yet',
       'pic': 'https://picsum.photos/300/30',
-      'message': 'I love to code'
+      'message': 'Add the first Comment'
     },
-    {
-      'name': 'Biggi Man',
-      'pic': 'https://picsum.photos/300/30',
-      'message': 'Very cool'
-    },
-    {
-      'name': 'Biggi Man',
-      'pic': 'https://picsum.photos/300/30',
-      'message': 'Very cool'
-    },
-    {
-      'name': 'Biggi Man',
-      'pic': 'https://picsum.photos/300/30',
-      'message': 'Very cool'
-    },
+
   ];
 
-  Widget commentChild(data) {
+  Widget commentChild(List data,String id) {
     return ListView(
       children: [
         for (var i = 0; i < data.length; i++)
@@ -73,6 +60,38 @@ class _CommentsState extends State<Comments> {
                 data[i]['name'],
                 style: TextStyle(fontWeight: FontWeight.bold),
               ),
+              trailing: data[i]['ownerId']== id ? PopupMenuButton(
+
+                onSelected: ( v) async{
+                  final flatId = ModalRoute.of(context)!.settings.arguments as String;
+                  print(v);
+                  if(v=='Delete')  {
+                    final http.Response response = await http.delete(
+                      Uri.parse
+                        ('https://afternoon-ridge-73830.herokuapp.com/posts/deleteComment/${data[i]['id']}/$id'),
+                      headers: <String, String>{
+                        'Content-Type': 'application/json; charset=UTF-8',
+                      },
+                    );
+                    print(response.statusCode);
+                    if(response.statusCode==200){
+                      print(response.body);
+
+                    }
+
+                  }
+
+                },
+                itemBuilder: (context) => [
+
+                  PopupMenuItem(
+                    child: Text('Delete'),
+                    value: 'Delete',
+
+                  ),
+                ],
+                icon: Icon(Icons.keyboard_arrow_down),
+              ) : Text(""),
               subtitle: Text(data[i]['message']),
             ),
           )
@@ -112,8 +131,10 @@ class _CommentsState extends State<Comments> {
 
                map['name'] = extractData['comment'][j][1];
                map['pic']= extractData['comment'][j][2];
-               print(extractData['comment'][j][2]);
                map['message']=extractData['comment'][j][0]['comment'];
+               map['id']=extractData['comment'][j][0]['_id'];
+               map['ownerId']=extractData['comment'][j][0]['ownerId'];
+               print(extractData['comment'][j][0]['_id']);
                loadedComments.add(map);
              }
             //
@@ -140,7 +161,8 @@ class _CommentsState extends State<Comments> {
 
   @override
   Widget build(BuildContext context) {
-
+    final flatId = ModalRoute.of(context)!.settings.arguments as String;
+    final currentuser = Provider.of<CurrentUserData>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text("Comments"),
@@ -148,20 +170,45 @@ class _CommentsState extends State<Comments> {
       ),
       body: Container(
         child: CommentBox(
-          userImage:
-          "https://lh3.googleusercontent.com/a-/AOh14GjRHcaendrf6gU5fPIVd8GIl1OgblrMMvGUoCBj4g=s400",
-          child: _isLoading ? Center(child: CircularProgressIndicator(color:Color(0xff1f95a1) ,),) :  commentChild(filedata),
+          userImage: currentuser.currentUserDate.img
+          ,
+          child: _isLoading ? Center(child: CircularProgressIndicator(color:Color(0xff1f95a1) ,),) :  commentChild(filedata,currentuser.currentUserDate.id),
           labelText: 'Write a comment...',
           withBorder: false,
           errorText: 'Plz Enter Your Comment',
           sendButtonMethod: () {
+            final currentuser = Provider.of<CurrentUserData>(context, listen: false);
+            final flatId = ModalRoute.of(context)!.settings.arguments as String;
+
+            Future<void> addComment(String comment) async {
+
+              final response = await http.post(
+                Uri.parse('https://afternoon-ridge-73830.herokuapp.com/posts/addComment/${flatId}/${currentuser.currentUserDate.id}'),
+                headers: <String, String>{
+                  'Content-Type': 'application/json; charset=UTF-8',
+                },
+                body: jsonEncode(<String, String>{
+                  'comment': comment,
+                }),
+              );
+              if(response.statusCode==200){
+                print('comment added');
+              }
+              else{
+                throw Exception('Failed to add comment');
+              }
+
+
+
+            }
             if (formKey.currentState!.validate()) {
+              addComment(commentController.text);
               print(commentController.text);
               setState(() {
                 var value = {
-                  'name': 'New User',
+                  'name': currentuser.currentUserDate.fName  + currentuser.currentUserDate.lName,
                   'pic':
-                  'https://lh3.googleusercontent.com/a-/AOh14GjRHcaendrf6gU5fPIVd8GIl1OgblrMMvGUoCBj4g=s400',
+                  currentuser.currentUserDate.img,
                   'message': commentController.text
                 };
                 filedata.insert(0, value);
