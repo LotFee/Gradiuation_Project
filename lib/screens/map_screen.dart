459338fbd44d94fe03186a5c958dab00b9359591
LotFee <lotfyua@gmail.com>
2022-05-16 01:37:35 +0200

    select map on location is done an current locatio

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -21,73 +21,84 @@ class _MapScreenState extends State<MapScreen> {
   var marker = HashSet<Marker>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text('Choose Your Location'),
-        backgroundColor: Color(0xff1f95a1),
-      ),
-      body: Stack(
-        children: [
-          GoogleMap(
-            onLongPress: (LatLng latLng) {
-              Marker markerr = marker.firstWhere(
-                (marker) => marker.markerId.value == "2",
-              );
-              setState(() {
-                marker.remove(markerr);
-              });
-            },
-            onTap: (LatLng latLng) {
-              var lat2 = lat;
-              var long2 = long;
-              setState(() {
-                lat = latLng.latitude;
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Text('Choose Your Location'),
+          backgroundColor: Color(0xff1f95a1),
+        ),
+        body: Stack(
+          children: [
+            GoogleMap(
+              onLongPress: (LatLng latLng) {
+                Marker markerr = marker.firstWhere(
+                  (marker) => marker.markerId.value == "2",
+                );
+                setState(() {
+                  marker.remove(markerr);
+                });
+              },
+              onTap: (LatLng latLng) {
+                var lat2 = lat;
+                var long2 = long;
+                setState(() {
+                  lat = latLng.latitude;
 
-                long = latLng.longitude;
+                  long = latLng.longitude;
 
-                print(lat.toString() + '   ' + long.toString());
-                marker.add(Marker(
-                    markerId: MarkerId('2'), position: LatLng(lat, long)));
-                if (lat != lat2 && long != long2) {
+                  print(lat.toString() + '   ' + long.toString());
                   marker.add(Marker(
                       markerId: MarkerId('2'), position: LatLng(lat, long)));
-                }
-              });
+                  if (lat != lat2 && long != long2) {
+                    marker.add(Marker(
+                        markerId: MarkerId('2'), position: LatLng(lat, long)));
+                  }
+                });
 
-            },
-            myLocationButtonEnabled: false,
-            zoomControlsEnabled: false,
-            onMapCreated: (controller) {
-              googlemap = controller;
-              setState(() {
-                marker.add(Marker(
-                    markerId: MarkerId('1'),
-                    position: LatLng(double.parse(widget.latitude), double.parse(widget.longitude))));
-              });
-            },
-            initialCameraPosition: CameraPosition(
-                target: LatLng(double.parse(widget.latitude), double.parse(widget.longitude)), zoom: 11.5),
-            markers: marker,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Container(
-                    alignment: Alignment.bottomRight,
-                    child: RaisedButton.icon(
-                        onPressed: () {
-                          String latlng = lat.toString() + ' '+ long.toString();
-                            Navigator.pop(context,latlng);
-                        },
-                        icon: Icon(
-                          Icons.location_on,
-                          color: Color(0xff1f95a1),
-                        ),
-                        label: Text('Confirm Your Location')))),
-          ),
-        ],
+              },
+              myLocationButtonEnabled: false,
+              zoomControlsEnabled: false,
+              onMapCreated: (controller) {
+                googlemap = controller;
+                setState(() {
+                  marker.add(Marker(
+                      markerId: MarkerId('1'),
+                      position: LatLng(double.parse(widget.latitude), double.parse(widget.longitude))));
+                });
+              },
+              initialCameraPosition: CameraPosition(
+                  target: LatLng(double.parse(widget.latitude), double.parse(widget.longitude)), zoom: 11.5),
+              markers: marker,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Container(
+                      alignment: Alignment.bottomRight,
+                      child: RaisedButton.icon(
+                          onPressed: () {
+                            if(lat!=0.0 && long!=0.0){
+                              String latlng = lat.toString() + ' '+ long.toString();
+                              Navigator.pop(context,latlng);
+                            }
+                            else
+                              {
+                                String latlng = widget.latitude + ' '+ widget.longitude;
+                                Navigator.pop(context,latlng);
+                              }
+
+                          },
+                          icon: Icon(
+                            Icons.location_on,
+                            color: Color(0xff1f95a1),
+                          ),
+                          label: Text('Confirm Your Location')))),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widget/location_input.dart
+++ lib/widget/location_input.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:location/location.dart';
@@ -35,99 +37,115 @@ class _LocationInputState extends State<LocationInput> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        foregroundColor: Color(0xff1f95a1),
-        backgroundColor: Colors.white,
-        backwardsCompatibility: false,
-        automaticallyImplyLeading: false,
-        systemOverlayStyle:
-            SystemUiOverlayStyle(statusBarColor: Color(0xff1f95a1)),
-        leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back_sharp,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          foregroundColor: Color(0xff1f95a1),
+          backgroundColor: Colors.white,
+          backwardsCompatibility: false,
+          automaticallyImplyLeading: false,
+          systemOverlayStyle:
+              SystemUiOverlayStyle(statusBarColor: Color(0xff1f95a1)),
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_sharp,
+            ),
+            onPressed: () {
+              String latlng = lat.toString() + ' ' + lon.toString();
+              Navigator.pop(context, latlng);
+            },
           ),
-          onPressed: () {
-            String latlng = lat.toString() + ' ' + lon.toString();
-            Navigator.pop(context, latlng);
-          },
+          title: Center(
+              child: Padding(
+            padding: const EdgeInsets.only(right: 45),
+            child: Text(
+              "Location",
+              style: TextStyle(
+                  color: Color(0xff1f95a1), fontWeight: FontWeight.bold),
+            ),
+          )),
         ),
-        title: Center(
-            child: Padding(
-          padding: const EdgeInsets.only(right: 45),
-          child: Text(
-            "Location",
-            style: TextStyle(
-                color: Color(0xff1f95a1), fontWeight: FontWeight.bold),
-          ),
-        )),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Container(
-            height: 350,
-            width: double.infinity,
-            child: _previewImage == ''
-                ? Text(
-                    'No Location Choosen',
-                    textAlign: TextAlign.center,
-                  )
-                : Container(
-              child: GoogleMap(
-                myLocationButtonEnabled: false,
-                zoomControlsEnabled: false,
-                initialCameraPosition: CameraPosition(
-                    target: LatLng(double.parse(lat), double.parse(lon)), zoom: 20),
-                markers: {
-                  Marker(markerId: MarkerId('1'),position: LatLng(double.parse(lat), double.parse(lon)))
-                },
-              ),
-            ) //Image.network(_previewImage,fit: BoxFit.cover,width: double.infinity,) ,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FlatButton.icon(
-                  onPressed: () {
-                    _getCurrentUserLocation();
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              height: 350,
+              width: double.infinity,
+              child: _previewImage == ''
+                  ? Text(
+                      'No Location Choosen',
+                      textAlign: TextAlign.center,
+                    )
+                  : Container(
+                child: GoogleMap(
+                  myLocationButtonEnabled: false,
+                  zoomControlsEnabled: false,
+                  initialCameraPosition: CameraPosition(
+                      target: LatLng(double.parse(lat), double.parse(lon)), zoom: 20),
+                  markers: {
+                    Marker(markerId: MarkerId('1'),position: LatLng(double.parse(lat), double.parse(lon)))
                   },
-                  icon: Icon(
-                    Icons.location_on,
-                    color: Color(0xff1f95a1),
-                  ),
-                  label: Text(
-                    'Current Location',
-                    style: TextStyle(
-                        color: Color(0xff1f95a1), fontWeight: FontWeight.bold),
-                  )),
-              FlatButton.icon(
-                  onPressed: () async {
-                    _getCurrentUserLocation();
-                    String latlng = await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                MapScreen(latitude: lat, longitude: lon)));
-                    setState(() {
-                      lat = latlng.split(" ").first;
-                      lon = latlng.split(" ").last;
-                      print(lat);
-                      print(lon);
-                    });
-                    // Navigator.push(context, MaterialPageRoute(builder: (context)=>MapScreen(latitude: lat, longitude: lon)));
-                  },
-                  icon: Icon(
-                    Icons.map,
-                    color: Color(0xff1f95a1),
-                  ),
-                  label: Text('Select on Map',
+                ),
+              ) //Image.network(_previewImage,fit: BoxFit.cover,width: double.infinity,) ,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FlatButton.icon(
+                    onPressed: () {
+                      _getCurrentUserLocation();
+                    },
+                    icon: Icon(
+                      Icons.location_on,
+                      color: Color(0xff1f95a1),
+                    ),
+                    label: Text(
+                      'Current Location',
                       style: TextStyle(
-                          color: Color(0xff1f95a1),
-                          fontWeight: FontWeight.bold))),
-            ],
-          ),
-        ],
+                          color: Color(0xff1f95a1), fontWeight: FontWeight.bold),
+                    )),
+                FlatButton.icon(
+                    onPressed: ()  {
+                      _getCurrentUserLocation();
+                      Timer(Duration(seconds: 2), ()async {
+                        String latlng = await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    MapScreen(latitude: lat, longitude: lon)));
+                        setState(() {
+                          lat = latlng.split(" ").first;
+                          lon = latlng.split(" ").last;
+                          print(lat);
+                          print(lon);
+                        });
+                      });
+                      // String latlng = await Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //         builder: (context) =>
+                      //             MapScreen(latitude: lat, longitude: lon)));
+                      // setState(() {
+                      //   lat = latlng.split(" ").first;
+                      //   lon = latlng.split(" ").last;
+                      //   print(lat);
+                      //   print(lon);
+                      // });
+                      // Navigator.push(context, MaterialPageRoute(builder: (context)=>MapScreen(latitude: lat, longitude: lon)));
+                    },
+                    icon: Icon(
+                      Icons.map,
+                      color: Color(0xff1f95a1),
+                    ),
+                    label: Text('Select on Map',
+                        style: TextStyle(
+                            color: Color(0xff1f95a1),
+                            fontWeight: FontWeight.bold))),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
